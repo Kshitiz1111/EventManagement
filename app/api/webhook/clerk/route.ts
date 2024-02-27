a95e9c7cc5cd89cf1a1fef7ad0b9eedd28e7e2c7
kshiitz1111 <kshitizghimire8@gmail.com>
2024-02-27 17:44:21 +0800

    third

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -54,42 +54,30 @@ export async function POST(req: Request) {
   const { id } = evt.data;
   const eventType = evt.type;
  
-  console.log(`Webhook with and ID of ${id} and type of ${eventType}`)
-  console.log('Webhook body:', body)
-  if(eventType === 'user.created'){
-    const {id, email_addresses, image_url, first_name, last_name, username} = evt.data;
+  if(eventType === 'user.created') {
+    const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
 
     const user = {
       clerkId: id,
       email: email_addresses[0].email_address,
-      /**
-       *  the exclamation mark (!) after username 
-       * is indeed the non-null assertion operator. 
-       * This operator is used to tell TypeScript
-       *  that the expression preceding it is not 
-       * null or undefined. It's a way to assert to
-       *  the TypeScript compiler that you're confident
-       *  the value will not be null or undefined at runtime,
-       */
       username: username!,
       firstName: first_name,
       lastName: last_name,
       photo: image_url,
     }
-
     const newUser = await createUser(user);
 
-    if(newUser){
-      await clerkClient.users.updateUserMetadata(id,{
-        publicMetadata:{
+    if(newUser) {
+      await clerkClient.users.updateUserMetadata(id, {
+        publicMetadata: {
           userId: newUser._id
         }
       })
     }
 
-    return NextResponse.json({message: 'OK', user: newUser}); 
+    return NextResponse.json({ message: 'OK', user: newUser })
   }
- 
+
   if (eventType === 'user.updated') {
     const {id, image_url, first_name, last_name, username } = evt.data
 
