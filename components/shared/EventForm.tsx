097ac41d84a78ae900f3591c597fe2544ba0e0e5
kshiitz1111 <kshitizghimire8@gmail.com>
2024-02-27 23:24:02 +0800

    --

---
+++ components/shared/EventForm.tsx
@@ -0,0 +1,15 @@
+'use client'
+import React from 'react'
+
+type EventFormProps = {
+   userId: string
+   type: "Create" | "Update"
+}
+
+const EventForm = ({ userId, type }: EventFormProps) => {
+   return (
+      <div>EventForm</div>
+   )
+}
+
+export default EventForm
