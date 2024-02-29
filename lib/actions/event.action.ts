50e301f923d99f9d2435ad8ce2a4167d68841b6b
kshiitz1111 <kshitizghimire8@gmail.com>
2024-02-29 22:24:29 +0800

    event create and get done

--- lib/actions/event.action.ts
+++ lib/actions/event.action.ts
@@ -1,11 +1,12 @@
 "use server"
 
-import { CreateEventParams } from "@/types"
+import { CreateEventParams, DeleteEventParams, GetAllEventsParams } from "@/types"
 import { handleError } from "../utils"
 import { connectToDatabase } from "../database"
 import User from "../database/models/user.model"
 import Event from "../database/models/even.model"
 import Category from "../database/models/category.actions"
+import { revalidatePath } from "next/cache"
 
 const populateEvent = async (query: any) =>{
    return query
@@ -45,6 +46,42 @@ export const getEventById = async (eventId: string) =>{
          throw new Error("Even not found");
       }
       return JSON.parse(JSON.stringify(event));
+   } catch (error) {
+      handleError(error);
+   }
+}
+
+export const getAllEvents = async ({query, limit=6, page, category}:GetAllEventsParams) =>{
+   try {
+      await connectToDatabase();
+      
+      const conditions = {}
+      const eventsQuery = Event.find(conditions)
+         .sort({createdAt: "desc"})
+         .skip(0)
+         .limit(limit);
+
+      const events = await populateEvent(eventsQuery);
+      const eventsCount = await Event.countDocuments(conditions);
+
+      return {
+         data: JSON.parse(JSON.stringify(events)),
+         totalPages: Math.ceil(eventsCount/limit),
+      }
+   } catch (error) {
+      handleError(error);
+   }
+}
+
+export const deleteEvent = async ({ eventId, path }:DeleteEventParams) =>{
+   try {
+      await connectToDatabase();
+
+      const deleatedEvent = await Event.findByIdAndDelete(eventId);
+
+      if(deleatedEvent){
+         revalidatePath(path);
+      }
    } catch (error) {
       handleError(error);
    }
